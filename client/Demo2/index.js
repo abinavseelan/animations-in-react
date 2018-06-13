bc93a69c64b5cfe55c60ab8eb503c1242e4b4a1c
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-06-13 18:13:25 +0530

    Demo 2

--- client/Demo2/index.js
+++ client/Demo2/index.js
@@ -1,11 +1,59 @@
 import React, { Component } from 'react';
+import { StaggeredMotion, spring } from 'react-motion';
 
-class Demo1 extends Component {
+class Demo2 extends Component {
     render() {
         return (
-            <p>Demo 1</p>
+            <StaggeredMotion
+                defaultStyles={[
+                    {
+                        left: 0,
+                        opacity: 0,
+                    },
+                    {
+                        left: 0,
+                        opacity: 0
+                    },
+                    {
+                        left: 0,
+                        opacity: 0
+                    },
+                ]}
+                styles={
+                    prevStyles => prevStyles.map((_, i) => {
+                        return i === 0
+                            ? {
+                                left: spring(window.innerWidth - 200, {
+                                    stiffness: 40,
+                                    dampening: 10,
+                                }),
+                                opacity: spring(1)
+                            }
+                            : { left: prevStyles[i-1].left, opacity: prevStyles[i-1].opacity }
+                    })
+                }
+            >
+                {
+                    styles => (
+                        <div>
+                            {
+                                styles.map((style, index) => (
+                                    <div
+                                        key={index}
+                                        className="box"
+                                        style={{
+                                            top: `${300*index}px`,
+                                            ...style
+                                        }}
+                                    />
+                                ))
+                            }
+                        </div>
+                    )
+                }
+            </StaggeredMotion>
         )
     }
 };
 
-export default Demo1;
+export default Demo2;
