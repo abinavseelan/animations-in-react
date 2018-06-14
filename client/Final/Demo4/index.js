485eeccb516bb1fc54b6a9dc283be63e00ef6f41
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-06-15 02:19:00 +0530

    Demo 4 - WIP

--- client/Final/Demo4/index.js
+++ client/Final/Demo4/index.js
@@ -1,11 +1,68 @@
 import React, { Component } from 'react';
+import contacts from './mock-details';
 
-class Demo1 extends Component {
+const ListView = ({ contacts, onClick }) => (
+    <div className="list-items">
+        {
+            contacts.map((contact, index) => (
+                <div className="list-item" key={contact._id} onClick={() => { onClick(index) }}>
+                    <div>
+                        <img src={contact.picture} />
+                    </div>
+                    <div>
+                        <h3>{`${contact.name.first} ${contact.name.last}`}</h3>
+                        <h4>{contact.phone}</h4>
+                    </div>
+                </div>
+            ))
+        }
+    </div>
+);
+
+const DetailView = ({ contact, onClick }) => (
+    <div className="item-details"></div>
+);
+
+class Demo4 extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            currentContact: null,
+        }
+
+        this.setCurrentContact = this.setCurrentContact.bind(this);
+    }
+
+    setCurrentContact(index) {
+        this.setState({
+            currentContact: contact[index],
+        });
+    }
+
+    back() {
+        this.setState({
+            currentContact: null,
+        });
+    }
+
     render() {
         return (
-            <p>Demo 1</p>
+            this.currentContact
+                ? (
+                    // <DetailView
+                    //     contact={this.state.currentItem}
+                    // />
+                    null
+                )
+                : (
+                    <ListView
+                        contacts={contacts}
+                        onClick={this.setCurrentContact}
+                    />
+                )
         )
     }
 };
 
-export default Demo1;
+export default Demo4;
---
+++ client/Final/Demo4/mock-details.js
@@ -0,0 +1,122 @@
+export default [
+  {
+    "_id": "5b22c6c94afaed067e49b271",
+    "index": 0,
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "name": {
+      "first": "Lynette",
+      "last": "Stanton"
+    },
+    "company": "TALAE",
+    "email": "lynette.stanton@talae.info",
+    "phone": "+1 (965) 538-2276",
+    "address": "340 Croton Loop, Fruitdale, Virginia, 9997",
+    "about": "Aliqua officia do adipisicing ipsum amet excepteur fugiat qui irure occaecat ad elit nulla. Elit consequat esse ullamco incididunt irure incididunt Lorem proident qui. Cupidatat deserunt nostrud cillum id ea in commodo sit nulla fugiat commodo ad sit adipisicing. Irure adipisicing Lorem irure aliquip mollit quis laborum aute nostrud cillum. Dolor et aliqua elit dolore. Non duis id aliquip sint mollit eu enim cupidatat fugiat do deserunt quis."
+  },
+  {
+    "_id": "5b22c6c9b6f375f7bbabf58c",
+    "index": 1,
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "name": {
+      "first": "Johnnie",
+      "last": "Valencia"
+    },
+    "company": "KINETICUT",
+    "email": "johnnie.valencia@kineticut.us",
+    "phone": "+1 (987) 465-3938",
+    "address": "696 Brigham Street, Hannasville, Puerto Rico, 3888",
+    "about": "Excepteur non dolore irure aute nulla esse dolor do commodo ad consequat duis. Excepteur aliquip ullamco eu labore. Irure Lorem sint officia exercitation consequat."
+  },
+  {
+    "_id": "5b22c6c925c16d767024f992",
+    "index": 2,
+    "picture": "http://placehold.it/32x32",
+    "age": 38,
+    "name": {
+      "first": "Conway",
+      "last": "Barlow"
+    },
+    "company": "EPLODE",
+    "email": "conway.barlow@eplode.org",
+    "phone": "+1 (908) 454-3055",
+    "address": "612 Allen Avenue, Marion, Rhode Island, 1518",
+    "about": "Do velit aute enim anim minim. Dolor eu nisi aliqua ad do nisi ad et officia irure laboris. Sunt dolore occaecat pariatur occaecat consectetur amet. Do elit id qui culpa esse aliquip ut. Nostrud deserunt commodo cillum tempor quis duis dolore consectetur aliqua enim. Cupidatat culpa nostrud fugiat quis aute est cillum."
+  },
+  {
+    "_id": "5b22c6c9881c5c6713530413",
+    "index": 3,
+    "picture": "http://placehold.it/32x32",
+    "age": 37,
+    "name": {
+      "first": "Donovan",
+      "last": "Ellis"
+    },
+    "company": "ENDIPINE",
+    "email": "donovan.ellis@endipine.me",
+    "phone": "+1 (927) 588-2479",
+    "address": "811 Chester Court, Maplewood, North Carolina, 5766",
+    "about": "Ut excepteur amet sit veniam sit consectetur culpa dolor enim voluptate. Excepteur reprehenderit exercitation id fugiat sunt ullamco sit tempor quis eu cupidatat enim dolor duis. Ex adipisicing irure non deserunt dolore proident nulla elit et id labore cupidatat."
+  },
+  {
+    "_id": "5b22c6c9b8772e976ede3cf7",
+    "index": 4,
+    "picture": "http://placehold.it/32x32",
+    "age": 31,
+    "name": {
+      "first": "Cochran",
+      "last": "Good"
+    },
+    "company": "KIGGLE",
+    "email": "cochran.good@kiggle.biz",
+    "phone": "+1 (980) 441-2607",
+    "address": "913 Conklin Avenue, Wadsworth, Arkansas, 3585",
+    "about": "Aliquip laboris labore sint est velit esse Lorem dolor eu incididunt ullamco ut sit. Occaecat esse sint id proident voluptate cupidatat labore amet tempor excepteur. Magna consectetur veniam nostrud ullamco eiusmod consequat voluptate nostrud eu occaecat nostrud ea exercitation est."
+  },
+  {
+    "_id": "5b22c6c91873c5a7c18b9c0e",
+    "index": 5,
+    "picture": "http://placehold.it/32x32",
+    "age": 21,
+    "name": {
+      "first": "Mcfadden",
+      "last": "Tanner"
+    },
+    "company": "SIGNIDYNE",
+    "email": "mcfadden.tanner@signidyne.com",
+    "phone": "+1 (882) 429-3878",
+    "address": "429 Pierrepont Place, Caledonia, Wyoming, 1341",
+    "about": "Eiusmod labore est labore culpa. Ad culpa quis ipsum in ullamco elit cillum dolore aliqua amet. Reprehenderit occaecat duis tempor irure veniam est labore aliqua duis enim occaecat enim. Eiusmod ad aute et commodo cupidatat et aliqua deserunt dolor labore Lorem adipisicing minim non. Irure voluptate anim sunt veniam. Tempor sint elit nisi exercitation veniam officia adipisicing elit tempor."
+  },
+  {
+    "_id": "5b22c6c9b5be37568c2c796a",
+    "index": 6,
+    "picture": "http://placehold.it/32x32",
+    "age": 36,
+    "name": {
+      "first": "Myra",
+      "last": "Cox"
+    },
+    "company": "EQUITAX",
+    "email": "myra.cox@equitax.co.uk",
+    "phone": "+1 (836) 428-2385",
+    "address": "115 Love Lane, Fairfield, Kentucky, 4231",
+    "about": "Officia officia ullamco cillum non veniam exercitation irure cupidatat. Duis ipsum dolore aliquip id aute cupidatat velit excepteur Lorem voluptate excepteur pariatur laborum minim. Veniam do sunt quis excepteur adipisicing non aute laborum. Ut eu nostrud excepteur quis cillum. Irure do irure do magna deserunt qui nostrud esse duis Lorem exercitation reprehenderit deserunt."
+  },
+  {
+    "_id": "5b22c6c9279d971d7a7efc50",
+    "index": 7,
+    "picture": "http://placehold.it/32x32",
+    "age": 35,
+    "name": {
+      "first": "Brewer",
+      "last": "Murray"
+    },
+    "company": "ENERVATE",
+    "email": "brewer.murray@enervate.biz",
+    "phone": "+1 (970) 583-2474",
+    "address": "882 Balfour Place, Graball, Texas, 8760",
+    "about": "Enim dolore enim tempor magna esse anim pariatur commodo sunt. Ex nisi duis nisi minim cillum aliquip elit. Esse ea magna dolor nulla eu. Occaecat id excepteur mollit quis reprehenderit enim. Commodo ipsum exercitation nulla consequat non aute esse deserunt reprehenderit culpa ullamco reprehenderit adipisicing."
+  }
+]
